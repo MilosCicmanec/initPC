24d7af2e30e270f637daeec232d64697182b8392
MilosCicmanec <171834077+MilosCicmanec@users.noreply.github.com>
2026-08-29 16:53:00 +0200

    gsettings - clipboard-history topbar-preview-size 30->8

--- DistroAgnostic/gnome/50extensions_gsettings.sh
+++ DistroAgnostic/gnome/50extensions_gsettings.sh
@@ -51,7 +51,7 @@ for_each "gsettings set org.gnome.shell.extensions.clipboard-history " << 'BASH'
     prev-entry "['<Shift><Alt>v']"
     toggle-menu "['<Super>v']"
     toggle-private-mode "[]"
-    topbar-preview-size "30"
+    topbar-preview-size "8"
     window-width-percentage "100"
 BASH
 
